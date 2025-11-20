1a3574a67f209a2335d15f74909bb4a5897f618c
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2025-11-19 20:50:16 -0600

    Grid added

--- tictactoe_python.py
+++ tictactoe_python.py
@@ -4,9 +4,17 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screenHeight = 720
+screenWidth = 720
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
 running = True
+
+lineLength = 600
+lineWidth = 6
+leftPadding = 240
+topPadding = 80
+#spaceOfLines = 50
 
 while running:
     # poll for events
@@ -16,9 +24,15 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
+    screen.fill("black")
 
     # RENDER YOUR GAME HERE
+    
+    pygame.draw.rect(screen, pygame.Color(255,255,255), pygame.Rect((screenWidth-(leftPadding)), (0+(topPadding)), lineWidth, lineLength), 6)
+    pygame.draw.rect(screen, pygame.Color(255,255,255), pygame.Rect((0+(leftPadding)), (0+(topPadding)), lineWidth, lineLength), 6)
+    pygame.draw.rect(screen, pygame.Color(255,255,255), pygame.Rect(( (screenHeight/2) - (lineLength/2) ), (0+(leftPadding)), lineLength, lineWidth), 6)
+    pygame.draw.rect(screen, pygame.Color(255,255,255), pygame.Rect(( (screenHeight/2) - (lineLength/2) ), (screenHeight-(leftPadding)), lineLength, lineWidth), 6)
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
